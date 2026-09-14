a52ca8093fd5e56fb41824bb13de779fa60e12ea
lbarreto-tech <lucaslbcs2003@gmail.com>
2026-09-14 11:05:50 -0300

    Projeto criado

---
+++ sistemaagendamentoescolar/src/main/java/com/projetotcc/sistemaagendamentoescolar/SistemaagendamentoescolarApplication.java
@@ -0,0 +1,13 @@
+package com.projetotcc.sistemaagendamentoescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaagendamentoescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaagendamentoescolarApplication.class, args);
+	}
+
+}
